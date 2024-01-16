90b3ea254a2b7ac5b34b1d0be18710bc298bc146
Taeyoung Won <hexter1994@gmail.com>
2024-01-16 17:23:12 +0900

    [Build] Beer 타겟 생성

---
+++ Beer/Presenter/Recommend/RecommendBeerViewController.swift
@@ -0,0 +1,19 @@
+//
+//  RecommendBeerViewController.swift
+//  Beer
+//
+//  Created by 원태영 on 1/16/24.
+//
+
+import UIKit
+
+final class RecommendBeerViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
